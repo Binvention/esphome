ec781f3710f551c83461326794a56a6f18dfa0ec
binvention <131bairdbk+work@gmail.com>
2025-02-26 15:22:52 -0700

    Fix dac setup

--- esphome/components/dac3100/dac3100.cpp
+++ esphome/components/dac3100/dac3100.cpp
@@ -25,14 +25,23 @@ void DAC3100::setup() {
   ERROR_CHECK(this->write_byte(DAC3100_SW_RST, 0x01), "Software reset failed");
   // *** Program clock settings ***
   //sets the clock to the bclk
-  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL1,0x01),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL1,0x07),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL3,0x20),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL4,0x0),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL5,0x0),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_CLK_PLL2,0x96),"Clock Config Failed");
+
   // Power up NDAC and set to 2
-  ERROR_CHECK(this->write_byte(DAC3100_NDAC, 0x82), "Set NDAC failed");
+  ERROR_CHECK(this->write_byte(DAC3100_NDAC, 0x84), "Set NDAC failed");
   // Power up MDAC and set to 2
-  ERROR_CHECK(this->write_byte(DAC3100_MDAC, 0x82), "Set MDAC failed");
-  //ERROR_CHECK(this->write_byte(DAC3100_CODEC_IF_1, 0x00), "Set CODEC_IF failed"); //defaults to i2s 16bit
+  ERROR_CHECK(this->write_byte(DAC3100_MDAC, 0x86), "Set MDAC failed");
+
+  ERROR_CHECK(this->write_byte(DAC3100_DOSR_MSB,0x01),"Clock Config Failed");
+  ERROR_CHECK(this->write_byte(DAC3100_DOSR,0x00),"Clock Config Failed");
+
+  ERROR_CHECK(this->write_byte(DAC3100_CODEC_IF_1, 0x00), "Set CODEC_IF failed"); //defaults to i2s 16bit
   // Program the DAC processing block to be used - PRB_P1
-  //ERROR_CHECK(this->write_byte(DAC3100_DAC_SIG_PROC, 0x01), "Set DAC_SIG_PROC failed"); //defaults to PRB_P1
+  ERROR_CHECK(this->write_byte(DAC3100_DAC_SIG_PROC, 0x19), "Set DAC_SIG_PROC failed"); //defaults to PRB_P1
   
   //setup dac datapath for left data through both dacs
   ERROR_CHECK(this->write_byte(DAC3100_DAC_DATA_SET,0x18), "Set DAC_DATA_SET failed");
@@ -46,19 +55,39 @@ void DAC3100::setup() {
   ERROR_CHECK(this->write_byte(DAC3100_DAC_OUT_MIX, 0x88), "Set DAC_OUT_MIX failed");
 
   // 
-  // ERROR_CHECK(this->write_byte(DAC3100_HP_POP_REM, 0X4E), "Set DAC3100_HP_POP_REM");//default should be fine
-  // Unmute drivers
-  ERROR_CHECK(this->write_byte(DAC3100_HPL_DRIVER, 0x06), "Set HPL_DRIVER failed");
-  ERROR_CHECK(this->write_byte(DAC3100_HPR_DRIVER, 0x06), "Set HPR_DRIVER failed");
-  ERROR_CHECK(this->write_byte(DAC3100_SPK_DRIVER, 0x04), "Set SPK_DRIVER failed");
+  ERROR_CHECK(this->write_byte(DAC3100_HP_POP_REM, 0X4E), "Set DAC3100_HP_POP_REM failed");
+  ERROR_CHECK(this->write_byte(DAC3100_PGA_RAMP_DOWN, 0X70), "Set DAC3100_HP_POP_REM failed");
+  ERROR_CHECK(this->write_byte(DAC3100_DAC_OUT_MIX,0x44),"Set output mixing failed")
+  ERROR_CHECK(this->write_byte(DAC3100_MIC_BIAS,0x0B),"Set mic bias failed")
+  ERROR_CHECK(this->write_byte(DAC3100_HP_DRIVER_CTRL,0xE0),"Set output mixing failed")
+  ERROR_CHECK(this->write_byte(DAC3100_L_TO_SPK_AN_VOL, 0x40), "Set driver volumefailed");
+  ERROR_CHECK(this->write_byte(DAC3100_HPL_AN_VOL, 0x92), "Set driver volume failed");
+  ERROR_CHECK(this->write_byte(DAC3100_HPR_AN_VOL, 0x92), "Set driver volume failed");
+  ERROR_CHECK(this->write_byte(DAC3100_HPL_DRIVER, 0x02), "Set HPL_DRIVER failed");
+  ERROR_CHECK(this->write_byte(DAC3100_HPR_DRIVER, 0x02), "Set HPR_DRIVER failed");
+  ERROR_CHECK(this->write_byte(DAC3100_SPK_DRIVER, 0x00), "Set SPK_DRIVER failed");
+  ERROR_CHECK(this->write_byte(DAC3100_SPK_AMP, 0x86), "Set HPL_DRIVER failed");
   
-  // Power up the drivers
-  ERROR_CHECK(this->write_byte(DAC3100_HP_DRIVER, 0xc4), "Set HP_DRIVER failed");
-  ERROR_CHECK(this->write_byte(DAC3100_SPK_AMP, 0x86), "Set SPK_AMP failed");
+
+  // *** Select Page 3
+  ERROR_CHECK(this->write_byte(DAC3100_PAGE_CTRL, 0x03), "Set page 3 failed");
+  ERROR_CHECK(this->write_byte(DAC3100_TIMER_MCLK_DIV, 0x01), "Set timer mclk div failed");
+
+
+  
+  // // Power up the drivers
+  // ERROR_CHECK(this->write_byte(DAC3100_HP_DRIVER, 0xc4), "Set HP_DRIVER failed");
+  // ERROR_CHECK(this->write_byte(DAC3100_SPK_AMP, 0x86), "Set SPK_AMP failed");
   // *** Select Page 0 *** 
   ERROR_CHECK(this->write_byte(DAC3100_PAGE_CTRL, 0x00), "Set page 1 failed");
+  ERROR_CHECK(this->write_byte(DAC3100_HEADSET_DETECT, 0x8C), "Set headset detection failed");
+  ERROR_CHECK(this->write_byte(DAC3100_INT_CTRL_1, 0x80), "Set interrupt control failed");
+  ERROR_CHECK(this->write_byte(DAC3100_GPIO_1_CTRL, 0x14), "Set page 1 failed");
+
+
+
   //power up the dac   
-  ERROR_CHECK(this->write_byte(DAC3100_DAC_DATA_SET,0xd4), "Set DAC_DATA_SET failed");
+  ERROR_CHECK(this->write_byte(DAC3100_DAC_DATA_SET,0xd9), "Set DAC_DATA_SET failed");
   // Set left and right DAC digital volume control
   ERROR_CHECK(this->write_volume_(), "Set volume failed");
   // Unmute left and right channels
