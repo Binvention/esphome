ea63cd463633185240165da42ab6bc128be9ae6e
binvention <131bairdbk+work@gmail.com>
2025-04-09 07:50:30 -0600

    make references explicit

--- esphome/components/trf7962a/trf7962a.cpp
+++ esphome/components/trf7962a/trf7962a.cpp
@@ -18,7 +18,7 @@ void TRF7962A::setup() {
   this->irq_pin_->setup();
   this->send_command(TRF7962A_CMD::SOFT_INIT);
   this->send_command(TRF7962A_CMD::IDLING);
-  set_timeout(50,[this](){
+  this->set_timeout(50,[this](){
     this->send_command(TRF7962A_CMD::RESET_FIFO);
     this->write_register(TRF7962A_REG::ISO_CONTROL,0b10000010);
     this->write_register(TRF7962A_REG::COL_POS_IRQ_MASK,0b00111110);
@@ -27,11 +27,11 @@ void TRF7962A::setup() {
     this->write_register(TRF7962A_REG::CHIP_STAT,0b00100001);
   });
   this->transfer_status_ = TRANSFER_STATUS::NO_TRANSACTIONS;
-  set_interval("get_random",1000,[this](){
-    ISO15693_get_random_slixl_();
+  this->set_interval("get_random",1000,[this](){
+    this->ISO15693_get_random_slixl_();
     ESP_LOGD(TAG,"Chip stat %02x",this->read_register(CHIP_STAT));
     ESP_LOGD(TAG,"FIFO STAT %02x",this->read_register(FIFO_STAT));
-    write_register(CHIP_STAT,0x21);
+    this->write_register(CHIP_STAT,0x21);
     ESP_LOGD(TAG,"Chip stat %02x",this->read_register(CHIP_STAT));
   });
 }
@@ -45,30 +45,30 @@ void TRF7962A::loop() {
   //check interrupts
   if(irq_pin_->digital_read()){
     ESP_LOGD(TAG,"IRQ Pin high");
-    enable();
-    write_byte(IRQ_STAT|READ);
-    set_mode(read_mode_); //switch to reading mode
-    uint8_t irq = read_byte();
-    read_byte(); //dummy read needed to clear irq
-    set_mode(write_mode_);//set back to write mode
-    disable();
+    this->enable();
+    this->write_byte(IRQ_STAT|READ);
+    this->set_mode(read_mode_); //switch to reading mode
+    uint8_t irq = this->read_byte();
+    this->read_byte(); //dummy read needed to clear irq
+    this->set_mode(write_mode_);//set back to write mode
+    this->disable();
     if(irq) {
       ESP_LOGD(TAG,"IRQ received"+irq);
       if(irq & TRF7962A_IRQ_STAT::RX_COMPLETE) {
-        uint8_t length = read_register(TRF7962A_REG::FIFO_STAT);
+        uint8_t length = this->read_register(TRF7962A_REG::FIFO_STAT);
         if (length & 0x10) {
           ESP_LOGE(TAG, "Error FIFO overflow detected");
         }
-        read_rx_bytes(length&0x0f);
+        this->read_rx_bytes(length&0x0f);
         transfer_status_ = TRANSFER_STATUS::RECEIVE_COMPLETE;
       }
       else if (irq & TRF7962A_IRQ_STAT::FIFO_HIGH_OR_LOW) {
-        uint8_t length = read_register(TRF7962A_REG::FIFO_STAT);
+        uint8_t length = this->read_register(TRF7962A_REG::FIFO_STAT);
         if (length & 0x10) {
           ESP_LOGE(TAG, "Error FIFO overflow detected");
         }
         if(length & 0x40) {
-          read_rx_bytes (length &0x0f);
+          this->read_rx_bytes (length &0x0f);
         }
       }
     }
@@ -114,42 +114,42 @@ void TRF7962A::loop() {
 }
 
 void TRF7962A::send_command(TRF7962A_CMD command) {
-  enable();
-  write_byte(command);
-  write_byte(0);
-  disable();
+  this->enable();
+  this->write_byte(command);
+  this->write_byte(0);
+  this->disable();
 }
 
 uint8_t TRF7962A::read_register(TRF7962A_REG reg){
   uint8_t data;
-  enable();
-  write_byte(reg | TRF7962A_TRANS_TYPE::READ);
-  set_mode(read_mode_); //set to read mode
-  data = read_byte();
-  set_mode(write_mode_); //set back to write mode
-  disable();
+  this->enable();
+  this->write_byte(reg | TRF7962A_TRANS_TYPE::READ);
+  this->set_mode(read_mode_); //set to read mode
+  data = this->read_byte();
+  this->set_mode(write_mode_); //set back to write mode
+  this->disable();
   ESP_LOGVV(TAG, "read_register_(%d) -> %d", reg, data);
   return data;
 }
 
 void TRF7962A::write_register(TRF7962A_REG reg, uint8_t value){
-  enable();
-  write_byte(reg);
-  write_byte(value);
-  disable();
+  this->enable();
+  this->write_byte(reg);
+  this->write_byte(value);
+  this->disable();
   ESP_LOGVV(TAG, "write_register_(%d,%d)",reg,value);
 }
 
 void TRF7962A::read_rx_bytes(uint8_t length) {
-  enable();
-  write_byte(TRF7962A_REG::FIFO_IO_REG | TRF7962A_TRANS_TYPE::READ | TRF7962A_TRANS_TYPE::CONTINUOUS);
-  set_mode(read_mode_);
+  this->enable();
+  this->write_byte(TRF7962A_REG::FIFO_IO_REG | TRF7962A_TRANS_TYPE::READ | TRF7962A_TRANS_TYPE::CONTINUOUS);
+  this->set_mode(read_mode_);
   for(uint8_t i = 0; i <= length; i++) {
-    rx_buff_.push_back(read_byte());
-    ESP_LOGD(TAG,"byte received %02x",rx_buff_.back());
+    this->rx_buff_.push_back(read_byte());
+    ESP_LOGD(TAG,"byte received %02x",this->rx_buff_.back());
   }
-  set_mode(write_mode_);
-  disable();
+  this->set_mode(write_mode_);
+  this->disable();
 }
 
 
@@ -162,19 +162,19 @@ ISO15693_RESULT TRF7962A::get_last_result(){
 }
 
 TRANSFER_STATUS TRF7962A::get_last_transfer_status(){
-  return transfer_status_;
+  return this->transfer_status_;
 }
 
 
 void TRF7962A::turn_field_off_(){
   this->write_register(TRF7962A_REG::CHIP_STAT,0X01);
-  field_on_ = false;
+  this->field_on_ = false;
   ESP_LOGD(TAG, "field off");
 }
 
 void TRF7962A::turn_field_on_(){
   this->write_register(TRF7962A_REG::CHIP_STAT,0X21);
-  field_on_ = true;
+  this->field_on_ = true;
   ESP_LOGD(TAG, "field on");
 }
 
@@ -183,17 +183,17 @@ ISO15693_RESULT TRF7962A::ISO15693_send_single_slot_inventory_(uint8_t* uid){
 }
 
 void TRF7962A::ISO15693_get_random_slixl_(){
-  enable();
-  write_byte(TRF7962A_CMD::RESET_FIFO); 
-  write_byte(TRF7962A_CMD::TRANSMIT_CRC); 
-  write_byte(TRF7962A_REG::TX_LEN_B1|TRF7962A_TRANS_TYPE::CONTINUOUS);
-  write_byte(0x00);
-  write_byte(0x30);
-  write_byte(0x02);//ISO15693_REQ_DATARATE_HIGH
-  write_byte(0xB2); //get random number
-  write_byte(0x04); //NXP manufacturer 
-  disable();
-  transfer_status_ = TRANSFER_STATUS::RECEIVE_WAIT;
+  this->enable();
+  this->write_byte(TRF7962A_CMD::RESET_FIFO); 
+  this->write_byte(TRF7962A_CMD::TRANSMIT_CRC); 
+  this->write_byte(TRF7962A_REG::TX_LEN_B1|TRF7962A_TRANS_TYPE::CONTINUOUS);
+  this->write_byte(0x00);
+  this->write_byte(0x30);
+  this->write_byte(0x02);//ISO15693_REQ_DATARATE_HIGH
+  this->write_byte(0xB2); //get random number
+  this->write_byte(0x04); //NXP manufacturer 
+  this->disable();
+  this->transfer_status_ = TRANSFER_STATUS::RECEIVE_WAIT;
   ESP_LOGD(TAG, "send get random number to SLIXL");
 }
 
