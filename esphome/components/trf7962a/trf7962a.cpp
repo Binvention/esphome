cbb4456220157032e9cdf5fddbb288315148781e
binvention <131bairdbk+work@gmail.com>
2025-04-23 17:38:32 -0600

    adjust reading rx bytes to try and get more consistent results

--- esphome/components/trf7962a/trf7962a.cpp
+++ esphome/components/trf7962a/trf7962a.cpp
@@ -129,8 +129,9 @@ void TRF7962A::read_rx_bytes(uint8_t length) {
                    (uint8_t) TRF7962A_TRANS_TYPE::CONTINUOUS);
   this->set_mode(read_mode_);
   for (uint8_t i = 0; i <= length; i++) {
-    this->rx_buff_.push_back(read_byte());
-    ESP_LOGD(TAG, "byte received %02x", this->rx_buff_.back());
+    uint8_t temp = read_byte();
+    this->rx_buff_.push_back(temp);
+    ESP_LOGVV(TAG, "byte received %02x", this->rx_buff_.back());
   }
   this->set_mode(write_mode_);
   this->disable();
