8f56dd27a79c4b3db11ed9b1d7578e1f86ef0a1b
binvention <131bairdbk+work@gmail.com>
2025-04-23 20:23:38 -0600

    change verbosity for debugging

--- esphome/components/trf7962a/trf7962a.cpp
+++ esphome/components/trf7962a/trf7962a.cpp
@@ -133,7 +133,7 @@ void TRF7962A::read_rx_bytes(uint8_t length) {
   for (uint8_t i = 0; i <= length; i++) {
     uint8_t temp = read_byte();
     this->rx_buff_.push_back(temp);
-    ESP_LOGVV(TAG, "byte received %02x", this->rx_buff_.back());
+    ESP_LOGD(TAG, "byte received %02x", this->rx_buff_.back());
   }
   this->set_mode(write_mode_);
   this->disable();
