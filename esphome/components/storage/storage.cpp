24c9dfa336fe573932b9a662fd6372b58e3ec993
binvention <131bairdbk+work@gmail.com>
2025-06-08 12:42:21 -0600

    add generic functions so that it doesn't need to be changed later for when buffers are added

--- esphome/components/storage/storage.cpp
+++ esphome/components/storage/storage.cpp
@@ -10,46 +10,50 @@ namespace storage {
 
 static const char *const TAG = "storage";
 FileInfo::FileInfo(String const &path, size_t size, bool is_directory)
-    : path(path), size(size), is_directory(is_directory) {}
+    : path(path), size(size), is_directory(is_directory) {
+  this->read_offset = 0;
+}
 
-// uint8_t Storage::read() {
-//    uint8_t data;
-//    data = this->read_byte(this->current_offset_);
-//    this->update_offset();
-//    return data;
-// }
+void Storage::set_file(String file) {
+  this->current_file_ = this->get_file_info(file);
+  if (this->current_file_.path == "") {
+    this->current_file_.path = file;
+  }
+  this->current_file_.read_offset = 0;
+  this->direct_set_file(file);
+}
 
-// void Storage::write(uint8_t data) {
-//    if(this->buffer_ != nullptr) {
-//       this->buffer_[this->current_offset_] = data;
-//    } else {
-//       ESP_LOGW(TAG, "Buffer not available using direct access instead");
-//       this->direct_write_byte(this->base_offset_+this->current_offset_,data);
-//    }
-//    this->update_offset();
-// }
+uint8_t Storage::read() {
+  uint8_t data;
+  data = this->direct_read_byte(this->current_file_.read_offset);
+  this->update_offset(1);
+  return data;
+}
 
-// uint8_t Storage::read_byte(uint32_t offset) {
-//    uint8_t data;
-//    if(this->buffer_ != nullptr) {
-//       data = this->buffer_[offset];
-//    }
-//    else {
-//       ESP_LOGW(TAG, "Buffer not available using direct access instead");
-//       data = this->direct_read_byte(this->base_offset_+offset);
-//    }
-//    return data;
-// }
+bool Storage::write(uint8_t data) {
+  //    if(this->buffer_ != nullptr) {
+  //       this->buffer_[this->current_offset_] = data;
+  //    } else {
+  ESP_LOGW(TAG, "Buffer not available using direct access instead");
+  return this->direct_write_byte(data);
+  //    }
+}
 
-// void Storage::write_byte(uint32_t offset, uint8_t data) {
-//    if(this->buffer_ != nullptr) {
-//       this->buffer_[offset] = data;
-//    } else {
-//       ESP_LOGW(TAG, "Buffer not available using direct access instead");
-//       this->direct_write_byte(this->base_offset_+offset,data);
-//    }
-// }
+bool Storage::append(uint8_t data) { return this->direct_append_byte(data); }
 
+size_t Storage::read_array(uint8_t *data, size_t data_length) {
+  size_t num_bytes_read = this->direct_read_byte_array(this->current_file_.read_offset, data, data_length);
+  this->update_offset(num_bytes_read);
+  return num_bytes_read;
+}
+
+bool Storage::write_array(uint8_t *data, size_t data_length) {
+  return this->direct_write_byte_array(data, data_length);
+}
+
+bool Storage::append_array(uint8_t *data, size_t data_length) {
+  return this->direct_write_byte_array(data, data_length);
+}
 // void Storage::allocate_buffer(uint32_t buffer_size) {
 //    if(buffer_size) {
 //       if(this->buffer_ == nullptr) {
@@ -62,24 +66,26 @@ FileInfo::FileInfo(String const &path, size_t size, bool is_directory)
 //    }
 // }
 
-// void Storage::update_offset() {
-//    this->current_offset_++;
-//    if (this->current_offset_ > this->buffer_size_){
-//       if(this->max_offset_ && (this->base_offset_ + this->current_offset_ > this->max_offset_)) {
-//          if(this->buffer_offset_ != this->base_offset_){
-//             this->load_buffer(base_offset_,0,this->buffer_size_);
-//          }
-//       } else {
-//          uint32_t new_buffer_offset = this->buffer_offset_+this->current_offset_;
-//          if(this->max_offset_ && (new_buffer_offset+this->buffer_size_-1 > this->max_offset_)) {
-//             this->load_buffer(this->buffer_offset_+this->current_offset_,0,this->max_offset_-new_buffer_offset+1);
-//          } else {
-//             this->load_buffer(this->buffer_offset_+this->current_offset_,0,this->buffer_size_);
-//          }
-//       }
-//       this->current_offset_ = 0;
-//    }
-// }
+void Storage::update_offset(size_t value) {
+  this->current_file_.read_offset += value;
+  //    if (this->current_offset_ > this->buffer_size_){
+  //       if(this->max_offset_ && (this->base_offset_ + this->current_offset_ > this->max_offset_)) {
+  //          if(this->buffer_offset_ != this->base_offset_){
+  //             this->load_buffer(base_offset_,0,this->buffer_size_);
+  //          }
+  //       } else {
+  //          uint32_t new_buffer_offset = this->buffer_offset_+this->current_offset_;
+  //          if(this->max_offset_ && (new_buffer_offset+this->buffer_size_-1 > this->max_offset_)) {
+  //             this->load_buffer(this->buffer_offset_+this->current_offset_,0,this->max_offset_-new_buffer_offset+1);
+  //          } else {
+  //             this->load_buffer(this->buffer_offset_+this->current_offset_,0,this->buffer_size_);
+  //          }
+  //       }
+  //       this->current_offset_ = 0;
+  //    }
+}
+
+void Storage::set_read_offset(size_t offset) { this->current_file_.read_offset = offset; }
 
 }  // namespace storage
 }  // namespace esphome
--- esphome/components/storage/storage.h
+++ esphome/components/storage/storage.h
@@ -11,27 +11,29 @@ struct FileInfo {
   String path;
   size_t size;
   bool is_directory;
-
+  size_t read_offset;
   FileInfo(String const &path, size_t size, bool is_directory);
 };
 
 class Storage : public EntityBase {
  public:
-  virtual uint8_t direct_read_byte(uint32_t offset);
+  // direct functions
+  virtual uint8_t direct_read_byte(size_t offset);
   virtual bool direct_write_byte(uint8_t data);
   virtual bool direct_append_byte(uint8_t data);
-  virtual size_t direct_read_byte_array(uint32_t offset, uint8_t *data, uint32_t data_length);
-  virtual bool direct_write_byte_array(uint8_t *data, uint32_t data_length);
-  virtual bool direct_append_byte_array(uint8_t *data, uint32_t data_length);
-  virtual void set_file(String file);
+  virtual size_t direct_read_byte_array(size_t offset, uint8_t *data, size_t data_length);
+  virtual bool direct_write_byte_array(uint8_t *data, size_t data_length);
+  virtual bool direct_append_byte_array(uint8_t *data, size_t data_length);
   virtual std::vector<FileInfo> list_directory(String path);
   virtual FileInfo get_file_info(String path);
-  // uint8_t read();
-  // void write(uint8_t data);
-  // uint8_t read_byte (uint32_t offset);
-  // void write_byte (uint32_t offset, uint8_t data);
-  // void read_byte_array(uint32_t offset, uint8_t * data, uint32_t data_length);
-  // void write_byte_array(uint32_t offset, uint8_t * data, uint32_t data_length);
+  void set_file(String file);
+  uint8_t read();
+  void set_read_offset(size_t offset);
+  bool write(uint8_t data);
+  bool append(uint8_t data);
+  size_t read_array(uint8_t *data, size_t data_length);
+  bool write_array(uint8_t *data, size_t data_length);
+  bool append_array(uint8_t *data, size_t data_length);
   // void write_buffer();
   // void refresh_buffer(uint32_t offset = 0);
   // uint8_t & operator[] (size_t index);
@@ -39,14 +41,15 @@ class Storage : public EntityBase {
   // void write_on_shutdown(bool value);
 
  protected:
+  virtual void direct_set_file(String file);
   // void load_buffer (uint32_t offset, uint32_t buffer_offset, uint32_t length);
   // void write_buffer (uint32_t offset, uint32_t buffer_offset, uint32_t length);
   // void allocate_buffer(uint32_t buffer_size);
-  // void update_offset();
+  void update_offset(size_t value);
   // uint8_t * buffer_;
   // uint32_t buffer_size_;
   // uint32_t buffer_offset_;
-  // uint32_t current_offset_;
+  FileInfo current_file_;
   // uint32_t base_offset_;
   // uint32_t max_offset_;
   // bool write_on_shutdown_;
