0c6224625bbc1b6ad0a821f0a0ed3670ce9e91fe
binvention <131bairdbk+work@gmail.com>
2025-07-30 08:57:41 -0600

    fix buffer available space check

--- esphome/components/audio/audio_reader.cpp
+++ esphome/components/audio/audio_reader.cpp
@@ -74,23 +74,29 @@ AudioFileType AudioReader::get_audio_type(const char *content_type) {
 AudioReaderState AudioReader::read() {
   uint8_t temp[TEMP_BUFFER_SIZE];
   int num_bytes;
-  int available = this->file_ring_buffer_->available();
-  do {
-    if (available >= TEMP_BUFFER_SIZE) {
-      num_bytes = this->storage_client_.read_array(&(temp[0]), TEMP_BUFFER_SIZE);
-    } else {
-      num_bytes = this->storage_client_.read_array(&(temp[0]), available ? available : 10);
-    }
-    available -= num_bytes;
-    if (num_bytes) {
-      size_t bytes_written =
-          this->file_ring_buffer_->write_without_replacement(temp, num_bytes, pdMS_TO_TICKS(READ_WRITE_TIMEOUT_MS));
-      if (bytes_written != num_bytes) {
-        ESP_LOGE("Audio Reader", "error occurred while writing to file buffer. Bytes Written %0d. Bytes in buffer %0d",
-                 bytes_written, num_bytes);
+  int available = this->file_ring_buffer_->free();
+  if (available > 0) {
+    do {
+      if (available >= TEMP_BUFFER_SIZE) {
+        num_bytes = this->storage_client_.read_array(&(temp[0]), TEMP_BUFFER_SIZE);
+      } else {
+        num_bytes = this->storage_client_.read_array(&(temp[0]), available);
       }
-    }
-  } while (num_bytes != 0 && available >= TEMP_BUFFER_SIZE);
+      available -= num_bytes;
+      if (num_bytes) {
+        size_t bytes_written =
+            this->file_ring_buffer_->write_without_replacement(temp, num_bytes, pdMS_TO_TICKS(READ_WRITE_TIMEOUT_MS));
+        if (bytes_written != num_bytes) {
+          ESP_LOGE("Audio Reader",
+                   "error occurred while writing to file buffer. Bytes Written %0d. Bytes in buffer %0d", bytes_written,
+                   num_bytes);
+        }
+      }
+    } while (num_bytes != 0 && available >= TEMP_BUFFER_SIZE);
+  } else {
+    ESP_LOGW("Audio Reader", "No Free Space in buffer");
+    return AudioReaderState::READING;
+  }
   if (num_bytes) {
     return AudioReaderState::READING;
   }
