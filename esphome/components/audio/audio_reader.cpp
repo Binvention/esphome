6e52bce9939dc66cd96d597355bb4ff2f10dd9d0
Brandon Baird <131bairdbk+work@gmail.com>
2025-07-30 16:45:16 -0600

    fix typo in constructor

--- esphome/components/audio/audio_reader.cpp
+++ esphome/components/audio/audio_reader.cpp
@@ -17,7 +17,7 @@ namespace audio {
 static const uint32_t READ_WRITE_TIMEOUT_MS = 20;
 static const size_t TEMP_BUFFER_SIZE = 50;
 
-AudioReader::AudioReader(size_t buffer_size) : buffer_size_(buffer_size);
+AudioReader::AudioReader(size_t buffer_size) : buffer_size_(buffer_size){}
 
 AudioReader::~AudioReader() {}
 
